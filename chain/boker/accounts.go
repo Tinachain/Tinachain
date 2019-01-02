f6ca31e97c48dadc1287739ef6cd7824e553aa37
张超 <513479626@qq.com>
2019-01-02 16:04:58 +0800

    Tianchain 1.0.0

---
+++ chain/boker/accounts.go
@@ -0,0 +1,141 @@
+//播客链增加的特殊账号管理类
+package boker
+
+import (
+	"github.com/Tinachain/Tina/chain/boker/protocol"
+	"github.com/Tinachain/Tina/chain/common"
+	"github.com/Tinachain/Tina/chain/log"
+)
+
+//帐号常量
+const DeloyAddress = "0xd7fd311c8f97349670963d87f37a68794dfa80ff"
+const SystemAddress = "0xd7fd311c8f97349670963d87f37a68794dfa80ff"
+
+var (
+	DeployPersonalContractAddress = common.HexToAddress(DeloyAddress)  //部署用户基础合约帐号
+	DeploySystemContractAddress   = common.HexToAddress(DeloyAddress)  //部署系统基础合约帐号
+	SetValidatorAddress           = common.HexToAddress(SystemAddress) //设置验证人账户
+	CommunityAddress              = common.HexToAddress(SystemAddress) //社区账户
+	FoundationAddress             = common.HexToAddress(SystemAddress) //基金账户
+	TeamAddress                   = common.HexToAddress(SystemAddress) //团队账户
+)
+
+//播客链的账号管理
+type AcccountLevel struct {
+	level []protocol.TxType
+}
+type BokerAccount struct {
+	accounts map[common.Address]AcccountLevel
+}
+
+func NewAccount() *BokerAccount {
+
+	bokerAccount := new(BokerAccount)
+	bokerAccount.accounts = make(map[common.Address]AcccountLevel)
+
+	//加载发布投票合约账户
+	log.Info("deployPersonalContractAccount")
+	bokerAccount.deployPersonalContractAccount()
+
+	//加载发布分币合约账户
+	log.Info("deploySystemContractAccount")
+	bokerAccount.deploySystemContractAccount()
+
+	//加载设置验证人账户
+	log.Info("loadSetValidator")
+	bokerAccount.loadSetValidator()
+
+	//加载基金会账户
+	log.Info("loadCommunityAccount")
+	bokerAccount.loadCommunityAccount()
+
+	return bokerAccount
+}
+
+func (a *BokerAccount) deployPersonalContractAccount() {
+
+	accountLevel := AcccountLevel{
+		level: make([]protocol.TxType, 0, 0),
+	}
+	accountLevel.level = append(accountLevel.level, protocol.SetPersonalContract)
+	accountLevel.level = append(accountLevel.level, protocol.CancelPersonalContract)
+	a.accounts[DeployPersonalContractAddress] = accountLevel
+}
+
+func (a *BokerAccount) deploySystemContractAccount() {
+
+	accountLevel := AcccountLevel{
+		level: make([]protocol.TxType, 0, 0),
+	}
+	accountLevel.level = append(accountLevel.level, protocol.SetSystemContract)
+	accountLevel.level = append(accountLevel.level, protocol.CancelSystemContract)
+	a.accounts[DeploySystemContractAddress] = accountLevel
+}
+
+func (a *BokerAccount) loadSetValidator() {
+
+	//加载添加验证人账号
+	accountLevel := AcccountLevel{
+		level: make([]protocol.TxType, 0, 0),
+	}
+	accountLevel.level = append(accountLevel.level, protocol.SetValidator)
+	a.accounts[SetValidatorAddress] = accountLevel
+}
+
+func (a *BokerAccount) IsValidator(address common.Address) bool {
+
+	if len(a.accounts) <= 0 {
+		return false
+	}
+
+	levels := a.accounts[address]
+	if len(levels.level) <= 0 {
+		return false
+	}
+
+	for _, v := range levels.level {
+		if v == protocol.SetValidator {
+			return true
+		}
+	}
+	return false
+}
+
+func (a *BokerAccount) loadCommunityAccount() {
+
+	//社区运营基金账户
+	communityOperationsAccount := AcccountLevel{
+		level: make([]protocol.TxType, 0, 0),
+	}
+	communityOperationsAccount.level = append(communityOperationsAccount.level, protocol.SetValidator)
+	a.accounts[CommunityAddress] = communityOperationsAccount
+
+	//基金会账户
+	foundationAccount := AcccountLevel{
+		level: make([]protocol.TxType, 0, 0),
+	}
+	foundationAccount.level = append(foundationAccount.level, protocol.SetValidator)
+	a.accounts[FoundationAddress] = foundationAccount
+
+	//团队账户
+	teamAccount := AcccountLevel{
+		level: make([]protocol.TxType, 0, 0),
+	}
+	teamAccount.level = append(teamAccount.level, protocol.SetValidator)
+	a.accounts[TeamAddress] = teamAccount
+}
+
+func (a *BokerAccount) GetAccount(account common.Address) ([]protocol.TxType, error) {
+
+	if len(a.accounts) > 0 {
+		value, exist := a.accounts[account]
+		if exist {
+			return value.level, nil
+		}
+	}
+
+	//测试使用
+	return []protocol.TxType{protocol.SetValidator}, nil
+
+	//return []protocol.TxType{protocol.Binary}, protocol.ErrSpecialAccount
+}
---
+++ chain/core/block_validator.go
@@ -0,0 +1,201 @@
+package core
+
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/Tinachain/Tina/chain/common/math"
+	"github.com/Tinachain/Tina/chain/consensus"
+	"github.com/Tinachain/Tina/chain/core/state"
+	"github.com/Tinachain/Tina/chain/core/types"
+	"github.com/Tinachain/Tina/chain/params"
+)
+
+type BlockValidator struct {
+	config *params.ChainConfig //链的配置信息
+	bc     *BlockChain         //规范块链
+	engine consensus.Engine    //验证采用的共识机制
+}
+
+var (
+	curGaslimit = big.NewFloat(0)
+	rtts        = big.NewFloat(0)
+	rttd        = big.NewFloat(0)
+)
+
+func NewBlockValidator(config *params.ChainConfig,
+	blockchain *BlockChain,
+	engine consensus.Engine) *BlockValidator {
+
+	return &BlockValidator{
+		config: config,
+		engine: engine,
+		bc:     blockchain,
+	}
+}
+
+//验证给定的区块叔块以及验证区块交易头
+func (v *BlockValidator) ValidateBody(block *types.Block) error {
+
+	//检查区块是否已知，如果不知道，它是否可链接
+	if v.bc.HasBlockAndState(block.Hash()) {
+		return ErrKnownBlock
+	}
+	if !v.bc.HasBlockAndState(block.ParentHash()) {
+		return consensus.ErrUnknownAncestor
+	}
+
+	//此时已知区块头有效性，检查叔块和交易
+	header := block.Header()
+	if err := v.engine.VerifyUncles(v.bc, block); err != nil {
+		return err
+	}
+	if hash := types.CalcUncleHash(block.Uncles()); hash != header.UncleHash {
+
+		return fmt.Errorf("uncle root hash mismatch: have %x, want %x", hash, header.UncleHash)
+	}
+	if hash := types.DeriveSha(block.Transactions()); hash != header.TxHash {
+
+		return fmt.Errorf("transaction root hash mismatch: have %x, want %x", hash, header.TxHash)
+	}
+	return nil
+}
+
+//验证者状态后发生的各种更改转换，例如用过的Gas，收据根和状态根本身 如果验证者成功，ValidateState将返回数据库批处理否则为nil并返回错误。
+func (v *BlockValidator) ValidateState(block, parent *types.Block,
+	statedb *state.StateDB,
+	receipts types.Receipts,
+	usedGas *big.Int) error {
+
+	header := block.Header()
+	if block.GasUsed().Cmp(usedGas) != 0 {
+		return fmt.Errorf("invalid gas used (remote: %v local: %v)", block.GasUsed(), usedGas)
+	}
+
+	// Validate the received block's bloom with the one derived from the generated receipts.
+	// For valid blocks this should always validate to true.
+	rbloom := types.CreateBloom(receipts)
+	if rbloom != header.Bloom {
+		return fmt.Errorf("invalid bloom (remote: %x  local: %x)", header.Bloom, rbloom)
+	}
+
+	// Tre receipt Trie's root (R = (Tr [[H1, R1], ... [Hn, R1]]))
+	receiptSha := types.DeriveSha(receipts)
+	if receiptSha != header.ReceiptHash {
+		return fmt.Errorf("invalid receipt root hash (remote: %x local: %x)", header.ReceiptHash, receiptSha)
+	}
+
+	// Validate the state root against the received state root and throw
+	// an error if they don't match.
+	if root := statedb.IntermediateRoot(v.config.IsEIP158(header.Number)); header.Root != root {
+		return fmt.Errorf("invalid merkle root (remote: %x local: %x)", header.Root, root)
+	}
+	return nil
+}
+
+func (v *BlockValidator) ValidateDposState(block *types.Block) error {
+
+	//验证区块中记录的Dpos根和区块头中记录的Dpos根是否一致
+	header := block.Header()
+	localRoot := block.DposCtx().Root()
+	remoteRoot := header.DposProto.Root()
+	if remoteRoot != localRoot {
+		return fmt.Errorf("invalid dpos root (remote: %x local: %x)", remoteRoot, localRoot)
+	}
+	return nil
+}
+
+func CalcGasLimit(parent *types.Block) *big.Int {
+
+	contrib := new(big.Int).Mul(parent.GasUsed(), big.NewInt(3))
+	contrib = contrib.Div(contrib, big.NewInt(2))
+	contrib = contrib.Div(contrib, params.GasLimitBoundDivisor)
+
+	//decay = parentGasLimit / 1024 -1
+	decay := new(big.Int).Div(parent.GasLimit(), params.GasLimitBoundDivisor)
+	decay.Sub(decay, big.NewInt(1))
+
+	gl := new(big.Int).Sub(parent.GasLimit(), decay)
+	gl = gl.Add(gl, contrib)
+
+	gl.Set(math.BigMax(gl, params.MinGasLimit))
+
+	if gl.Cmp(params.TargetGasLimit) < 0 {
+		gl.Add(parent.GasLimit(), decay)
+		gl.Set(math.BigMin(gl, params.TargetGasLimit))
+	}
+	return gl
+}
+
+//
+func bound(lower *big.Float, middle *big.Float, upper *big.Float) *big.Float {
+
+	if lower.Cmp(middle) == -1 {
+
+		//lower < middle
+		if middle.Cmp(upper) == -1 {
+
+			//middle < upper
+			return middle
+		} else {
+
+			//middle > upper
+			return upper
+		}
+	} else if lower.Cmp(middle) == 0 {
+
+		//lower == middle
+		if middle.Cmp(upper) == -1 {
+
+			//middle < upper
+			return middle
+		} else {
+
+			//middle > upper
+			return upper
+		}
+
+	} else {
+
+		//lower > middle
+		if lower.Cmp(upper) == -1 {
+
+			//lower < upper
+			return lower
+		} else {
+
+			//lower > upper
+			return upper
+		}
+	}
+}
+
+//计算门限阀值均值
+func calcSsthresh(gas *big.Int) *big.Int {
+
+	//求均值
+	gasFloat := new(big.Float).SetInt(gas)
+	if curGaslimit.Cmp(big.NewFloat(0)) == 0 {
+
+		rtts = gasFloat
+		rttd = rttd.Quo(rtts, big.NewFloat(2.0))
+	} else {
+
+		rtts = rtts.Add(rtts.Mul(rtts, big.NewFloat(0.875)), rtts.Mul(gasFloat, big.NewFloat(0.125)))
+		absFloat := new(big.Float).Mul(big.NewFloat(0.25), new(big.Float).Abs(new(big.Float).Sub(gasFloat, rtts)))
+		rttd = rttd.Add(rttd.Mul(rttd, big.NewFloat(0.75)), absFloat)
+	}
+	curGaslimit = curGaslimit.Add(rtts, new(big.Float).Mul(rttd, big.NewFloat(4.0)))
+	curGaslimit = bound(params.MinGasLimitFloat, curGaslimit, params.GasLimitSsthresh)
+
+	//转换成big.Int类型
+	ssthreshInt := new(big.Int)
+	curGaslimit.Int(ssthreshInt)
+	return ssthreshInt
+}
+
+//计算Gaslimit
+/*func CalcGasLimit(parent *types.Block) *big.Int {
+
+	return calcSsthresh(parent.GasUsed())
+}*/
