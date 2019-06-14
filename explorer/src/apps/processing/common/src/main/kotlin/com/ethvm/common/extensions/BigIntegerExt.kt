a64b0fa55d0ca8608ae02fe2d1fa953c5eaecb8f
Ethan(张超) <513479626@qq.com>
2019-06-14 10:47:47 +0800

    Tinachain 1.0.8

--- explorer/src/apps/processing/common/src/main/kotlin/com/ethvm/common/extensions/BigIntegerExt.kt
+++
@@ -1,8 +0,0 @@
-package com.ethvm.common.extensions
-
-import java.math.BigInteger
-import java.nio.ByteBuffer
-
-fun BigInteger.byteBuffer(): ByteBuffer = ByteBuffer.wrap(this.toByteArray())
-
-fun BigInteger.toHex() = "0x${this.toString(16)}"
--- explorer/src/apps/utils/src/commands/index.ts
+++
@@ -1,3 +0,0 @@
-export * from '@app/commands/coingecko/eth-tokens-list-to-coingecko-ids'
-export * from '@app/commands/verifier/erc20-balances'
-export * from '@app/commands/verifier/ether-balances'
